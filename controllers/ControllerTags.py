8cd7f9b417ccb38466736979f8ec2bac1e2748f9
Austriss <austriszeidmanis@gmail.com>
2025-04-17 13:12:22 +0300

    fix: function return fix

---
+++ controllers/ControllerTags.py
@@ -0,0 +1,80 @@
+import flask
+from flask import request, redirect, url_for, session, jsonify
+from loguru import logger
+
+from controllers.ControllerDatabase import ControllerDatabase
+
+from models.ModelTag import ModelTag
+
+
+class ControllerTags:
+    blueprint = flask.Blueprint("tags", __name__, url_prefix="/tags")
+
+    @staticmethod
+    @blueprint.route("/edit/<tag_id>", methods=["POST","GET"])
+    def tag_edit(tag_id):
+        return flask.render_template(
+            'tags/edit_tag.html'
+            )
+
+    @staticmethod
+    @blueprint.route("/new", methods=["POST", "GET"])
+    def new_tag():
+        if request.method == "POST":
+            tag = ModelTag()
+            data = request.get_json()
+            label = data["label"]
+            tag.label = label
+            success = ControllerDatabase.insert_tag(tag)
+            if success:
+                return redirect(url_for('tags.list_all_tags'))
+
+        return flask.render_template(
+            'tags/new.html'
+            )
+
+    @staticmethod
+    @blueprint.route('/get_tags', methods=["GET"])
+    def get_all_tags():
+        tags = ControllerDatabase.get_all_tags()
+        tags_json = jsonify(tags)
+        return tags_json
+
+    @staticmethod
+    @blueprint.route('/tags_list', methods=["GET"])
+    def list_all_tags():
+        return flask.render_template(
+            "tags/tags.html",
+            )
+
+    @staticmethod
+    @blueprint.route("/delete/<tag_id>", methods=["POST"])
+    def tag_delete(tag_id: int):
+        ControllerDatabase.delete_tag(tag_id)
+        return redirect(url_for(
+            'tags.list_all_tags'
+            ))
+
+    @staticmethod
+    @blueprint.route("/get_tag/<tag_id>", methods=["GET"])
+    def get_tag(tag_id: int = 0):
+        tag = ControllerDatabase.get_tag_from_id(tag_id)
+        tag_json = jsonify(tag)
+        return tag_json
+
+    @staticmethod
+    @blueprint.route("/update_tag/<tag_id>", methods=["POST"])
+    def update_tag(tag_id: int):
+        try:
+            tag = ModelTag()
+            tag.tag_id = tag_id
+            data_to_update = request.get_json()
+            new_label = data_to_update["label"]
+            tag.label = new_label
+            success = ControllerDatabase.update_tag(tag_id, tag)
+        except Exception as e:
+            logger.error(e)
+        return redirect(url_for(
+            'tags.list_all_tags'
+        ))
+
